83cf119c6f782f01e45acfffa67cd91e6fa9323c
Joao Guilherme Teixeira da Costa <jguilherme.tcosta@gmail.com>
2026-03-25 15:12:23 -0300

    ex05.js da lista de vetores

---
+++ lista-vetor-01/ex05.js
@@ -0,0 +1,9 @@
+let numbs = [2,8,9,10,3,4];
+let maior = numbs[0];
+
+for ( let i = 0 ; i < numbs.length; i++) {
+    if (numbs[i] > maior) {
+        maior = numbs[i]
+    }
+}
+  console.log(maior)
