4b268e0f068760f7573f9e8719379442fab23ab1
Joao Guilherme Teixeira da Costa <jguilherme.tcosta@gmail.com>
2026-03-26 15:34:13 -0300

    ex14.js lista vetor01

--- lista-vetor-01/ex14.js
+++ lista-vetor-01/ex14.js
@@ -1,12 +1,13 @@
 let a = [1, 2, 3, 4, 5, 6, 7, 8] // o x vai ser 7
-
+let b = 5
+let indice = 0;
 
 for (let i = 0; i < a.length; i++) {
-    if (a[i] === 7) {
-        console.log(a[i])
-    } else {
-        console.log("-1");
+    if (a[i] === b) {
+        indice = i
+        break
     }
 
 }
 
+console.log(indice)
