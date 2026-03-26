ea3011228ceff876489bc8d3f6924b01103940f6
Joao Guilherme Teixeira da Costa <jguilherme.tcosta@gmail.com>
2026-03-26 14:43:33 -0300

    ex14.js lista vetor01

---
+++ lista-vetor-01/ex14.js
@@ -0,0 +1,12 @@
+let a = [1, 2, 3, 4, 5, 6, 7, 8] // o x vai ser 7
+
+
+for (let i = 0; i < a.length; i++) {
+    if (a[i] === 7) {
+        console.log(a[i])
+    } else {
+        console.log("-1");
+    }
+
+}
+
